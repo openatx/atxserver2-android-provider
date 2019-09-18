12b057fdcd951d8a0c4c492d575b7beff36e5a43
codeskyblue <codeskyblue@gmail.com>
2019-09-18 13:37:41 +0800

    update docker install-adb.sh script

--- install-adb.sh
+++ install-adb.sh
@@ -6,6 +6,22 @@
 
 set -e
 
+SYSTEM=
+
+case "$(uname -s)" in
+	Linux)
+		SYSTEM="linux"
+		;;
+	Darwin)
+		echo "Unsupported Mac platform, run command: brew cask install android-platform-tools"
+		exit 1
+		;;
+	*)
+		echo "Unsupported system $(uname -s)"
+		exit 1
+		;;
+esac
+
 TAG=
 case "$(uname -m)" in
     x86_64)
@@ -20,7 +36,14 @@ case "$(uname -m)" in
         ;;
 esac
 
-cp vendor/multios-adbs/$TAG/adb /usr/local/bin/adb-tmp
+if ! which wget
+then
+	sudo apt-get install -y wget
+fi
+
+
+wget "https://github.com/openatx/adb-binaries/raw/master/1.0.40/$TAG/adb" -O /usr/local/bin/adb-tmp
+
 chmod +x /usr/local/bin/adb-tmp
 mv /usr/local/bin/adb-tmp /usr/local/bin/adb
 
@@ -28,5 +51,10 @@ if ! test -d /root/.android
 then
     mkdir -m 0750 /root/.android
 fi
+
+
+
 cp vendor/keys/adbkey /root/.android/adbkey
 cp vendor/keys/adbkey.pub /root/.android/adbkey.pub
+
+adb version
